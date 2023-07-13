8e4dc95f90a097ad4bc808e7e6e578917dea17c7
Dunaevd <dunaevskiy@bk.ru>
2023-07-14 01:47:58 +0300

    add function

--- index.php
+++ index.php
@@ -1,7 +1,38 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 echo "Hello";
 //phpinfo();
 $a = 5;
 $b = 6;
 $v = $a + $b;
+//рекурсия
+function recursion($num){
+    if ($num >= 10) {
+        ?><p><?=($num);?></p>
+        <?php 
+        $num--;
+        recursion($num);
+    }else echo '$num < 10';
+     return;
+}
 
+recursion(15);
+
+?>
+<hr>
+<?php
+
+function factorial($number){
+    if ($number <= 1) {
+        return 1;
+    }else return $number * factorial($number - 1);
+}
+
+echo factorial(10);
+
+//phpinfo();
+
+?>
